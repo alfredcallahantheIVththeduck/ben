b186a9955650b8be1d5729da19e9d314c1219cb7
Jason <63813682+pigs-pigs@users.noreply.github.com>
2022-05-24 13:56:30 -0400

    btn

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-$(".searchButton").click(function () {
+$("#searchButton").click(function () {
   console.log($(".searchTerm").val())
   window.location.href = "https://www.google.com/search?q=" + $(".searchTerm").val()
 }
 )
 
-$(".searchTerm").keypress(function (event) {
+$("#searchTerm").keypress(function (event) {
   var keycode = (event.keyCode ? event.keyCode : event.which);
   if (keycode == '13') {
     console.log($(".searchTerm").val())
