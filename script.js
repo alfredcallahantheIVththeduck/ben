04919466092caaeaa10fb04490070a5a7cd860ef
Jason <63813682+pigs-pigs@users.noreply.github.com>
2022-05-24 13:54:50 -0400

    Click

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-$(document).ready(function () {
-
-
-  $("body").css({
-    "background-repeat": "no-repeat",
-    "background-size": "cover"
-  })
-});
-
-
-
-$(".searchButton").click(function () { console.log($(".searchTerm").val())
+$(".searchButton").click(function () {
+  console.log($(".searchTerm").val())
   window.location.href = "https://www.google.com/search?q=" + $(".searchTerm").val()
 }
 )
@@ -17,6 +7,7 @@ $(".searchButton").click(function () { console.log($(".searchTerm").val())
 $(".searchTerm").keypress(function (event) {
   var keycode = (event.keyCode ? event.keyCode : event.which);
   if (keycode == '13') {
+    console.log($(".searchTerm").val())
     window.location.href = "https://www.google.com/search?q=" + $(".searchTerm").val()
   }
 });
