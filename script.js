ec492f49087ee44b35180c226e6055a0f2f6d978
Jason <63813682+pigs-pigs@users.noreply.github.com>
2022-05-24 14:00:34 -0400

    Click Fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $("#searchButton").click(function () {
   console.log($(".searchTerm").val())
-  window.location.href = "https://www.google.com/search?q=" + $(".searchTerm").val()
+  window.location.href = "https://www.google.com/search?q=" + $("#searchTerm").val()
 }
 )
 
@@ -8,6 +8,6 @@ $("#searchTerm").keypress(function (event) {
   var keycode = (event.keyCode ? event.keyCode : event.which);
   if (keycode == '13') {
     console.log($(".searchTerm").val())
-    window.location.href = "https://www.google.com/search?q=" + $(".searchTerm").val()
+    window.location.href = "https://www.google.com/search?q=" + $("#searchTerm").val()
   }
 });
