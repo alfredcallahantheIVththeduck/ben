65ac9ca404de49b94a643b6d3c9542061c45fb71
Jason <63813682+pigs-pigs@users.noreply.github.com>
2022-05-24 16:17:31 -0400

    Fix CSS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $("#searchButton").click(function () {
-  console.log($(".searchTerm").val())
-  window.location.href = "https://www.google.com/search?q=" + $("#searchTerm").val()
+  var search = $("#searchTerm").val()
+
 }
 )
 
@@ -8,6 +8,8 @@ $("#searchTerm").keypress(function (event) {
   var keycode = (event.keyCode ? event.keyCode : event.which);
   if (keycode == '13') {
     console.log($(".searchTerm").val())
-    window.location.href = "https://www.google.com/search?q=" + $("#searchTerm").val()
+    if (typeof (search) == "string") {
+      window.location.href = "https://www.google.com/search?q=" + search
+    }
   }
 });
