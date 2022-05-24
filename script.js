7a85c5b834ffa871f6e5105bbb3fb4e0f9daedc4
Jason <63813682+pigs-pigs@users.noreply.github.com>
2022-05-24 10:38:21 -0400

    Search

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-$("body").css({
-  "background-repeat": "no-repeat",
-  "background-size": "cover" })
+  $("body").css({
+    "background-repeat": "no-repeat",
+    "background-size": "cover"
+  })
 });
 
 
 
-$(".searchbuttobn").click(function(){
-window.location.href =  "https://www.google.com/search?q=" + $(".searchTerm").text
+$(".searchbuttobn").click(function () {
+  window.location.href = "https://www.google.com/search?q=" + $(".searchTerm").val()
 }
 )
+
+$(".searchbuttobn").keypress(function (event) {
+  var keycode = (event.keyCode ? event.keyCode : event.which);
+  if (keycode == '13') {
+    window.location.href = "https://www.google.com/search?q=" + $(".searchTerm").val()
+  }
+});
