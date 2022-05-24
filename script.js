3dd143a36dd2bb409a01984e02fc02c1b8b8faa5
Jason <63813682+pigs-pigs@users.noreply.github.com>
2022-05-24 16:32:34 -0400

    Search bar

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 $("#searchButton").click(function () {
   var search = $("#searchTerm").val()
-
-}
-)
+  if (typeof (search) == "string") {
+    window.location.href = "https://www.google.com/search?q=" + search
+  }
+})
 
 $("#searchTerm").keypress(function (event) {
   var keycode = (event.keyCode ? event.keyCode : event.which);
   if (keycode == '13') {
-    console.log($(".searchTerm").val())
+    console.log($("#searchTerm").val())
     if (typeof (search) == "string") {
       window.location.href = "https://www.google.com/search?q=" + search
     }
